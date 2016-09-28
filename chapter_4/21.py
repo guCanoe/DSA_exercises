84ec614a9594f20b871b5b85271ccb78ab116baf
guCanoe <juyou3hui@gmail.com>
2016-09-28 21:48:29 +0800

    oh oh oh

--- chapter_4/21.py
+++ chapter_4/21.py
@@ -1,2 +1,14 @@
 def find_sp(s, k, a, b):
-    if {}
+    if a>=b:
+        return
+    sp = s[a] + s[b]
+    if sp == k:
+        print [s[a], s[b]]
+        find_sp(s, k, a+1, b-1)
+    elif sp > k: 
+        find_sp(s, k, a, b-1)
+    else:
+        find_sp(s, k, a+1, b)
+            
+
+find_sp(range(50), 20, 0, 49)
