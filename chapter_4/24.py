d5f33707766538fe845cbefce0c5159d3f809f56
juyh <juyou3hui@gmail.com>
2016-09-28 21:21:58 +0800

    merge

---
+++ chapter_4/24.py
@@ -0,0 +1,12 @@
+def str2num(d, s):
+    r = 0
+    for i in range(len(s)):
+        r+=d[s[-(i+1)]]*10**i
+    return r
+    
+def sum_puzzle(a, b, c)
+    letters = set(list(a) + list(b) + list(c))
+
+def reange(unseted, seted):
+    for i in len(letter):
+        for 
