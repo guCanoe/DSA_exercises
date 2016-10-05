163050fea38b202700381a01616109f5b41538d5
guCanoe <juyou3hui@gmail.com>
2016-10-05 14:27:52 +0800

    chapter 4 completeed

--- chapter_4/24.py
+++ chapter_4/24.py
@@ -4,9 +4,22 @@ def str2num(d, s):
         r+=d[s[-(i+1)]]*10**i
     return r
     
-def sum_puzzle(a, b, c)
-    letters = set(list(a) + list(b) + list(c))
+a, b, c = "pot", "pan", "bib"
+letters = list(set(list(a) + list(b) + list(c)))
 
-def reange(unseted, seted):
-    for i in len(letter):
-        for 
+def sum_puzzle(d):
+    mapping = {letters[i] : d[i] for i in range(len(d))}
+    if str2num(mapping, a) + str2num(mapping, b) == str2num(mapping, c):
+        return True
+    else:
+        return False
+
+
+def reange(l, t=[], unseted=set(range(10)) ):
+    if not l :
+        if sum_puzzle(t):
+            print t
+    for num in unseted:
+        reange(l-1, t+[num], unseted-set([num])) 
+        
+reange(len(letters))
