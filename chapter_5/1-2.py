67a6cf7fbbea2306e6db138685f5f685022e342e
guCanoe <juyou3hui@gmail.com>
2016-10-05 15:25:26 +0800

    good

---
+++ chapter_5/1-2.py
@@ -0,0 +1,11 @@
+import sys
+data = []
+t = 0
+for k in range(100):
+    a = len(data)
+    b = sys.getsizeof(data)
+    data.append(None)
+    t = sys.getsizeof(data)
+    if not t == b:
+        print 'Length: {0:3d}; size in bytes {1:4d}'.format(a, b)
+    
